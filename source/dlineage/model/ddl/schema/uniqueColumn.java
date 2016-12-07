4a24aa31f906dda755e2c1682b362aa5f95d9f48
siwkon <konrad.siwakowski@teradata.com>
2016-12-07 15:40:07 +0100

    first version - not changed

---
+++ source/dlineage/model/ddl/schema/uniqueColumn.java
@@ -0,0 +1,19 @@
+package dlineage.model.ddl.schema;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+
+@Element(name="unique-column")
+public class uniqueColumn {
+
+	@Attribute(required = true)
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
